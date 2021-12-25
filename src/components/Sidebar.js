5ef1d5b140736ddd4f76488ba554d88d0339ca81
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2021-12-25 15:01:36 +0300

    add info for the app in modal

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,7 +21,7 @@ const Sidebar = () => {
       <nav className="menu">
         <ul>
           <li>
-            <a onClick={toggleModal}>About BrandColor</a>
+            <a onClick={toggleModal}>About APP</a>
           </li>
         </ul>
       </nav>
@@ -35,9 +35,9 @@ const Sidebar = () => {
         <button className="model-close-btn" onClick={toggleModal}>
           <GrClose />
         </button>
-        <h3>About Colors</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, magni temporibus. Tempora facilis accusamus dolorem laborum dolores, iure optio fugiat! s.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, magni temporibus. Tempora facilis accusamus dolorem laborum dolores, iure optio fugiat! s.</p>
+        <h3>About App</h3>
+        <p>First off all, this project cloned from brandcolors.net. </p>
+        <p>If you like the project, you can look at code base from <a target="_blank" href="https://github.com/codescaptain">My Github Profile</a></p>
 
       </Modal>
 
